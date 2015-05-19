4f5fd0425c0fc2fd73a6e52bdb5d952f58e4c273
skinnersBoxy <jor11235@gmail.com>
2015-05-18 20:56:19 -0400

    I don't know how to merge
    Merging is hard

--- src/Manager.java
+++ src/Manager.java
@@ -6,11 +6,8 @@ public class Manager {
 		Opponent b=new Basic_AI(1,1);
 		UI gui = new Local_GUI_3D();
 		Game g=new Game(a,b,gui,new TimeControl(5,0,0));
-<<<<<<< HEAD
 		new Intro(g,gui,a,b);
-=======
 		g.init();
 		gui.init(new Opponent[]{a,b}, new long[]{0,1}, g);
->>>>>>> b5a2703c6ceee81cac59278a605c48c40d8c0370
 	}
 }
